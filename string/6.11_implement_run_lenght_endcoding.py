6019122e51213cb35e38b565b25e297b90b4eb64
jun-hf <fong.junh@gmail.com>
2022-05-13 12:55:25 +0100

    added 6.11

---
+++ string/6.11_implement_run_lenght_endcoding.py
@@ -0,0 +1,37 @@
+"""
+Messages decode and encoding.
+
+"3e2c" = "eeecc"
+"eeecc" = "3e2c"
+
+"""
+def decoder(str):
+    count, s = 0, []
+    for i in range(len(str)):
+        if str[i].isdigit():
+            count = int(str[i])
+        else:
+            s.append(count*str[i])
+            count = 0
+    return ''.join(s)
+
+def encode(st):
+    count, result , char = 0, [], st[0]
+    for i in range(len(st)):
+        if st[i] == char:
+            count += 1
+        else:
+            result.append(str(count) + char)
+            count = 1
+            char = st[i]
+    result.append(str(count) + char)
+    return ''.join(result)
+
+
+
+
+
+print(decoder('2d3c'))
+print(encode('eeecc'))
+
+
